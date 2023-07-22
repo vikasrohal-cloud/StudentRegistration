e129471bb6213533f522c097333f993095209634
vikas choudhary <cvikas861@gmail.com>
2023-07-22 15:11:22 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace StudentRegistration
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----- Student Registration Form -----");
+
+            // Get student details from the user
+            Console.Write("Enter student's name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter student's age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter student's course: ");
+            string course = Console.ReadLine();
+
+            // Display the collected information
+            Console.WriteLine("\n--- Student Details ---");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Course: " + course);
+
+            Console.WriteLine("\nStudent registration successful!");
+        }
+    }
+}
